b1d9e5860d37fbe4932aa62d6ac7e33d115011ac
NicoMontoya <nmontoy03@gmail.com>
2019-07-29 17:24:05 -0700

    models for database

---
+++ how_can_you_help/models.py
@@ -0,0 +1,19 @@
+""" SQL Alchemy models for the app """
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+class Organization(DB.Model):
+    """ charity organization names """
+
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(50), nullable=False)
+    text = DB.Column(DB.Unicode(1500), nullable=False)
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    website = DB.Column(DB.String(50), nullable=False)
+    address = DB.Column(DB.String(50), nullable=False)
+    city = DB.Column(DB.String(20), nullable=False)
+    zip_code = DB.Column(DB.Integer, nullable=False)
+
+    def __repr__(self):
+        return "<Organization {}".format(self.name)
