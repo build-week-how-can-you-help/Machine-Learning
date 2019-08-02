8b77374ee4b34d5b00ea9164fd05f8f6bd7e3222
NicoMontoya <nmontoy03@gmail.com>
2019-08-02 11:29:48 -0700

    add CORS

--- how_can_you_help/app.py
+++ how_can_you_help/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request
 from .models import DB, Organization
 from .process import find_organizations
-
+from flask_cors import CORS, cross_origin
 
 
 
@@ -12,24 +12,26 @@ def create_app():
     """ create and configure instance of flask app """
 
     app = Flask(__name__)
-
+    CORS(app)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
     app.config['ENV'] = 'prod'
     DB.init_app(app)
-    
+
 
 
     @app.route("/")
+    @cross_origin()
     def root():
         return render_template("layout.html", title="CharityBW")
 
     @app.route("/find", methods=["POST"])
+    @cross_origin()
     def find():
-        
+
         description_text = request.values['description_text']
         finder = find_organizations(description_text)
         return finder
-    
+
 
 
     return app
